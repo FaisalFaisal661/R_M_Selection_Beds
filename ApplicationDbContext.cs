8a84cd0ba5c46729c5a9c7f7c1f375f5e3915e5b
FaisalFaisal661 <92310868+FaisalFaisal661@users.noreply.github.com>
2024-10-25 09:48:19 +0530

    InitialCommit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using R_M_Selection_Beds.Models;
+
+namespace R_M_Selection_Beds.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactFormModel> ContactForms { get; set; }
+    }
+}
